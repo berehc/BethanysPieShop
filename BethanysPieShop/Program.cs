b96e1e52a6973e4a1a072c2171e9604c895238b1
Berenice Hernández <obhernandezc@gbm.com.mx>
2023-09-14 18:27:26 -0600

    Adding middlewares

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,6 +1,31 @@
+// SERVICE REGISTRATION //
+using System.Runtime.CompilerServices;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews(); //enables MVC in my app
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//SETTING UP MIDDLEWARE //
+
+//app.MapGet("/", () => "Hello World!"); //listens for an incoming request to the root of the application and will return "Hello World"
+
+
+// middleware component that is preconfigured to look for incoming requests for static files, such as a JPEG or CSS file. It will look in that default configured folder wwwroot for static file and return it. It will then also short circuit the request
+app.UseStaticFiles();  
+
+
+//diagnostic middleware component that rwill not always show the exception page, but it´s called the DEveloper Exception Page.Contains information only for developers. That Page only will showed if the app is running in a Developer enviroment.
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+
+//middleware to add the ability to be able to navigate to our pages to make sure that ASP:NET Core will be able to handle incoming reuqest correctly. This will set again some defaults used typically in MVC to route to the views that we are going to have.
+// this is our endpoint middleware
+app.MapDefaultControllerRoute();
 
 app.Run();
